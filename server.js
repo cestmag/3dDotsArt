f5bc4592cf6063eed9f85b2df7e39f7b2f912cdd
cestmag <satoshi31622@gmail.com>
2022-03-15 13:07:03 +0900

    changed something

--- server.js
+++ server.js
@@ -9,7 +9,17 @@ app.get('/',(req, res) =>{
     res.render('recep.ejs')
 })
 app.get('/gallery',(req, res)=>{
-    res.render('index.ejs')
+    var version=req.query.ver
+    //console.log(version)
+    if(version=="static"){
+        res.render('indexx.ejs') 
+    }else if(version=="dynamic"){
+        res.render('index.ejs')
+    }
+    
+})
+app.get('/expla',(req, res)=>{
+    res.render('expl.ejs')
 })
 
 app.listen(process.env.PORT||3000)
